b596ff64b4233b90aa989c9a3c47f8786b1a6423
Anikesh Kumar <anikesh6203@gmail.com>
2025-04-18 17:16:41 +0530

    fix hireme scroll

--- src/components/navbar/NavbarBtn.jsx
+++ src/components/navbar/NavbarBtn.jsx
@@ -1,17 +1,22 @@
 import React from "react";
 import { LuArrowDownRight } from "react-icons/lu";
+import { Link } from "react-scroll";
 
 function NavbarBtn() {
   return (
-    <a
-      href="https://wa.me/+916203422416"
-      className="px-4 py-2 rounded-full text-xl font-bold text-white border-cyan border flex items-center gap-2 bg-gradient-to-r from-cyan to-orange hover:border-orange ShadowCs hover:scale-110 duration-300 cursor-pointer"
+    <Link
+      to="contact"
+      smooth={true}
+      spy={true}
+      duration={900}
+      offset={-140}
+      className="px-4 py-2 rounded-full text-xl font-bold text-[#fff] border-cyan border flex items-center gap-2 bg-gradient-to-r from-cyan to-orange hover:border-orange ShadowCs hover:scale-110 duration-300 cursor-pointer"
     >
       Hire Me
       <div className=" sm:hidden md:block">
         <LuArrowDownRight />
       </div>
-    </a>
+    </Link>
   );
 }
 
