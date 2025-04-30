195eb34208a3c847a3f376bd02f9be3a5267ce68
Anikesh Kumar <anikesh6203@gmail.com>
2025-04-30 17:29:25 +0530

    fix experience

--- src/components/experienceSection/ExperienceTopLeft.jsx
+++ src/components/experienceSection/ExperienceTopLeft.jsx
@@ -13,10 +13,10 @@ function ExperienceTopLeft() {
       className="flex flex-col gap-6 w-[300px]  "
     >
       <p className="text-orange font-bold uppercase text-3xl font-special text-center">
-        Since 2023
+        Since 2024
       </p>
       <div className="flex justify-center items-center gap-4 ">
-        <ExperienceInfo number="1" text="Years" />
+        <ExperienceInfo number="1+" text="Years" />
         <p className=" font-bold text-6xl text-lightBrown">-</p>
         <ExperienceInfo number="7" text="WebSites" />
       </div>
